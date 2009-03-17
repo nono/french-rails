3d37fab9f5b7757c2544502efd89a114570820ff
Bruno Michel <bmichel@menfin.info>
2009-03-17 23:40:38 +0100

    Added some comments

--- lib/french_rails/action_view/text_helper.rb
+++ lib/french_rails/action_view/text_helper.rb
@@ -3,9 +3,19 @@ require 'action_view/helpers/text_helper'
 module ActionView
   module Helpers
     module TextHelper
+
+      # Monkey-patch the pluralize from ActionView::Helpers::TextHelper
+      # to the french grammar rules (ie 0 is singular).
+      #
+      # Example:
+      #   pluralize(0, 'image') => 0 image
+      #   pluralize(1, 'image') => 1 image
+      #   pluralize(2, 'image') => 2 images
+      #
       def pluralize(count, singular, plural = nil)
         "#{count || 0} " + ((count.to_i <= 1) ? singular : (plural || singular.pluralize))
       end
+
     end
   end
 end
--- lib/french_rails/backend/simple.rb
+++ lib/french_rails/backend/simple.rb
@@ -3,6 +3,11 @@ require 'i18n/backend/simple'
 module FrenchRails
   module Backend
     class Simple < I18n::Backend::Simple
+
+      # The french grammar rules are not the same that for english.
+      # For our concern, the main difference is 0:
+      # in english, 0 is plural, but in french, it's singular.
+      #
       def pluralize(locale, entry, count)
         return entry unless entry.is_a?(Hash) and count
         key = :zero if count == 0 && entry.has_key?(:zero)
@@ -10,6 +15,7 @@ module FrenchRails
         raise InvalidPluralizationData.new(entry, count) unless entry.has_key?(key)
         entry[key]
       end
+
     end
   end
 end
