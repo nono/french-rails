7837478fad9ce4fd565b3795ffb021ba7ec12dcb
Bruno Michel <bmichel@menfin.info>
2010-06-09 12:37:01 +0200

    Fix pluralizations (i18n has changed its API)

--- lib/french_rails/backend/simple.rb
+++ lib/french_rails/backend/simple.rb
@@ -1,24 +1,23 @@
-require 'i18n/backend/base'
+require 'i18n/backend/simple'
 require 'i18n/backend/memoize'
 
 module FrenchRails
-  module Backend
-    class Simple
-      include I18n::Backend::Base
-      include I18n::Backend::Memoize
-
-      # The french grammar rules are not the same that for english.
-      # For our concern, the main difference is 0:
-      # in english, 0 is plural, but in french, it's singular.
-      #
-      def pluralize(locale, entry, count)
-        return entry unless entry.is_a?(Hash) and count
-        key = :zero if count == 0 && entry.has_key?(:zero)
-        key ||= count <= 1 ? :one : :other
-        raise InvalidPluralizationData.new(entry, count) unless entry.has_key?(key)
-        entry[key]
-      end
-
+  module Pluralization
+    # The french grammar rules are not the same that for english.
+    # For our concern, the main difference is 0:
+    # in english, 0 is plural, but in french, it's singular.
+    #
+    def pluralize(locale, entry, count)
+      return entry unless entry.is_a?(Hash) and count
+      key = :zero if count == 0 && entry.has_key?(:zero)
+      key ||= count <= 1 ? :one : :other
+      raise InvalidPluralizationData.new(entry, count) unless entry.has_key?(key)
+      entry[key]
     end
   end
+end
+
+class I18n::Backend::Simple
+  include FrenchRails::Pluralization
+  include I18n::Backend::Memoize
 end
--- lib/french_rails/i18n.rb
+++ lib/french_rails/i18n.rb
@@ -1,3 +1,2 @@
-I18n.backend = FrenchRails::Backend::Simple.new
 I18n.load_path << Dir[File.join(File.dirname(__FILE__), '..', '..', 'locales', '*.{rb,yml}')]
 I18n.default_locale = :fr
