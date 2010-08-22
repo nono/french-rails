712cae803d91f792b8b15d8a85a11afe03b11dd9
Bruno Michel <bmichel@menfin.info>
2010-08-22 16:53:09 +0200

    Fix a bug on loading locales

--- lib/french_rails/i18n.rb
+++ lib/french_rails/i18n.rb
@@ -1,2 +1,2 @@
-I18n.load_path << Dir[File.join(File.dirname(__FILE__), '..', '..', 'locales', '*.{rb,yml}')]
+I18n.load_path += Dir[File.join(File.dirname(__FILE__), '..', '..', 'locales', '*.{rb,yml}')]
 I18n.default_locale = :fr
