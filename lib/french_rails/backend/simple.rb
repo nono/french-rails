131291936e4e9eebb4ec07d8e8893ffb083399cc
Davy Defaud <davy.defaud@free.fr>
2020-02-16 22:21:07 +0100

    Fix French plural rule
    In French everything strictly above -2 and below 2 is singular (e.g. -1.99, -1, 0, 1, 1.99)

--- lib/french_rails/backend/simple.rb
+++ lib/french_rails/backend/simple.rb
@@ -3,14 +3,15 @@ require 'i18n/backend/memoize'
 
 module FrenchRails
   module Pluralization
-    # The french grammar rules are not the same that for english.
-    # For our concern, the main difference is 0:
-    # in english, 0 is plural, but in french, it's singular.
+    # The French grammar rules are not the same that for English.
+    # In english, everything different than 1 is plural.
+    # In French everything strictly above -2 and below 2 is singular
+    # e.g -1.99, -1, 0, 1, 1.99
     #
     def pluralize(locale, entry, count)
       return entry unless entry.is_a?(Hash) and count
       key = :zero if count == 0 && entry.has_key?(:zero)
-      key ||= count <= 1 ? :one : :other
+      key ||= count.abs < 2 ? :one : :other
       raise InvalidPluralizationData.new(entry, count) unless entry.has_key?(key)
       entry[key]
     end
